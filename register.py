d8510fc210920d23055007adaee5a7ba9488987e
Jason Fleagle <jasonfleagle@hotmail.com>
2020-09-03 21:12:39 -0400

    First commit

---
+++ register.py
@@ -0,0 +1,21 @@
+from tkinter import * 
+
+def register():
+	print("hello")
+
+def login():
+	print("Login session started")
+
+def main_screen():
+	screen = Tk()
+	screen.geometry("300x250")
+	screen.title("Notes 1.0")
+	Label(text = "Notes 1.0", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack()
+	Label(text = "").pack()
+	Button(text = "Login", height = "2", width = "30", command = login).pack()
+	Label(text = "").pack()
+	Button(text = "Register", height = "2", width = "30", command = register).pack()
+
+	screen.mainloop()
+
+main_screen()
