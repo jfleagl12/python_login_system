57746c7d2eeeefb83b6776fbd8379180304c5a1a
Jason Fleagle <jasonfleagle@hotmail.com>
2020-09-05 11:21:47 -0400

    Added in text output file based on username and field has password

--- register.py
+++ register.py
@@ -1,9 +1,28 @@
 from tkinter import * 
 
+def register_user(): 
+	username_info = username.get()
+	password_info = password.get()
+
+	file = open(username_info + ".txt", "w")
+	file.write(username_info)
+	file.write(password_info)
+	file.close
+
+	username_entry.delete(0, END)
+	password_entry.delete(0, END)
+
+	Label(text = "Registration Successful!")
+
 def register():
 	screen1 = Toplevel(screen)
 	screen1.title("Register")
 	screen1.geometry("300x250")
+
+	global username
+	global password 
+	global username_entry
+	global password_entry
 
 	username = StringVar()
 	password = StringVar()
@@ -11,11 +30,13 @@ def register():
 	Label(screen1, text = "Please enter details below").pack()
 	Label(screen1, text = "").pack()
 	Label(screen1, text = "Username * ").pack()
-	Entry(screen1, textvariable = username).pack()
+	username_entry = Entry(screen1, textvariable = username)
+	username_entry.pack()
 	Label(screen1, text = "Password * ").pack()
-	Entry(screen1, textvariable = password).pack()
+	password_entry = Entry(screen1, textvariable = password)
+	password_entry.pack()
 	Label(screen1, text = "").pack()
-	Button(screen1, text = "Register", width = 10, height = 1).pack()
+	Button(screen1, text = "Register", width = 10, height = 1, command=register_user).pack()
 
 
 def login():
